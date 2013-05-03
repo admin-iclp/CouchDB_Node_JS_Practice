b6de83fade6143c60a26387229f3bd5966ea6fdc
steven.sun <steven_sun@iclp.com.cn>
2013-05-03 14:31:48 +0800

    update

---
+++ Node_JS/Node_JS/hello_world.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+}).listen(process.env.PORT);
---
+++ Node_JS/Node_JS/server.js
@@ -0,0 +1,15 @@
+﻿var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        route(handle, pathname, response, request);
+    }
+
+    http.createServer(onRequest).listen(process.env.PORT);
+    console.log("Server has started.");
+}
+
+exports.start = start;
