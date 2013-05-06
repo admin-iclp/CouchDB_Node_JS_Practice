ce6671311c84ab382334560ef22c5b1df737517f
steven.sun <steven_sun@iclp.com.cn>
2013-05-06 16:46:13 +0800

    update

---
+++ MVC_RAZOR/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_RAZOR.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return "This is my <b>default</b> action..."; 
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        } 
+
+    }
+}
